d215025bdc57373ac601420dfa1a021526d21444
JustAbby <abbyikoedem78@gmail.com>
2022-06-10 17:03:52 +0200

    food site

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <NavBar />
+  <ContentSection />
+  <FooterPart />
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
+import FooterPart from '@/components/FooterPart.vue';
+import ContentSection from '@/components/ContentSection.vue';
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar,
+    FooterPart,
+    ContentSection,
+    
+}
+}
+</script>
